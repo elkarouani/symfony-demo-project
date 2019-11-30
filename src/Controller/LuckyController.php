f7b9b5380e8e98d01b94e600df2b71c658c41145
KDragon <elkarouani@gmail.com>
2019-11-30 21:59:47 +0100

    Task: adding my first route

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky", name="lucky")
+     */
+    public function index()
+    {
+        return $this->render('lucky/index.html.twig', [
+            'controller_name' => 'LuckyController',
+        ]);
+    }
+
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: ' . $number . '</body><html>'
+        );
+    }
+}
