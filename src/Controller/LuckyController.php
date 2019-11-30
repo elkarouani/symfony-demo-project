49b6e3f0fb46264c47269b0fb613e5a7f5469d27
KDragon <elkarouani@gmail.com>
2019-11-30 22:06:00 +0100

    Task: adding annotations bundle to the project

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
+// use Symfony\Component\Routing\Annotation\Route;
+
 class LuckyController extends AbstractController
 {
     /**
