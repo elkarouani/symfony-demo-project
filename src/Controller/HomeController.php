17e5d38a855f34eaaa013e16fd3de305232861fb
KDragon <elkarouani@gmail.com>
2019-12-01 21:37:56 +0100

    Task: adding my main pages folder with the home page and its controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class HomeController extends AbstractController
+{
+
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return new Response($this->twig->render('pages/home.html.twig'));
+    }
+}
